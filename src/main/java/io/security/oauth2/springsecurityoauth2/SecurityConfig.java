05e5aae9fcf362c7a0c1a8abed5791c719659310
inwoo0808 <inwoo0808@gmail.com>
2025-01-24 09:12:56 +0900

    feat: client 제작1

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -0,0 +1,33 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .defaultSuccessUrl("/", true)  // 로그인 성공 후 메인 페이지('/')로 리다이렉트
+                .failureUrl("/login?error=true");  // 로그인 실패시 리다이렉트
+
+        return http.build();
+    }
+}
